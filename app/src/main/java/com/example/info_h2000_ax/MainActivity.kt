f85720fe845c50a27906f8c49efefb80c554c7d9
LaPax615 <kabarega.peace.sheja@gmail.com>
2023-05-05 20:17:22 +0200

    Demi-partie prototype

--- app/src/main/java/com/example/info_h2000_ax/MainActivity.kt
+++ app/src/main/java/com/example/info_h2000_ax/MainActivity.kt
@@ -2,15 +2,47 @@ package com.example.info_h2000_ax
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    //val displayMetrics = resources.displayMetrics
+    //val dpiX = displayMetrics.xdpi
+    //val dpiY = displayMetrics.ydpi
+
+
+
     lateinit var drawingView: DrawingView
+
+    fun onClickUP(v: View) {
+        drawingView.player_up()
+        drawingView.invalidate()
+        println("Up Clicked")
+    }
+
+    fun onClickDOWN(v: View) {
+        drawingView.player_down()
+        drawingView.invalidate()
+        println("Down Clicked")
+    }
+
+    fun onClickRIGHT(v: View) {
+        drawingView.player_right()
+        drawingView.invalidate()
+        println("Right Clicked")
+    }
+
+    fun onClickLEFT(v: View) {
+        drawingView.player_left()
+        drawingView.invalidate()
+        println("Left Clicked")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        drawingView = findViewById<DrawingView>(R.id.surfaceView)
+        drawingView = findViewById<DrawingView>(R.id.vMain)
         drawingView.setWillNotDraw(false)
         drawingView.invalidate()
 
