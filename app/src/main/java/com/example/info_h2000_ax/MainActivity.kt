a1fbe849fd321346e0c57f841963706ee2074626
LaPax615 <kabarega.peace.sheja@gmail.com>
2023-05-08 11:31:15 +0200

    Demi-partie prototype 2

--- app/src/main/java/com/example/info_h2000_ax/MainActivity.kt
+++ app/src/main/java/com/example/info_h2000_ax/MainActivity.kt
@@ -1,8 +1,20 @@
 package com.example.info_h2000_ax
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+import java.util.*
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +25,12 @@ class MainActivity : AppCompatActivity() {
 
 
     lateinit var drawingView: DrawingView
+
+    fun new_Mazee(dim: Int) {
+        drawingView.new_maze(dim)
+        drawingView.invalidate()
+    }
+
 
     fun onClickUP(v: View) {
         drawingView.player_up()
@@ -38,13 +56,66 @@ class MainActivity : AppCompatActivity() {
         println("Left Clicked")
     }
 
+    fun Reset(v: View) {
+        drawingView.player_reset()
+        drawingView.invalidate()
+        println("Left Clicked")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
+        // Inflate the pop-up layout
+        val popupView = layoutInflater.inflate(R.layout.popup_layout, null)
+
+        // Create the AlertDialog to display the pop-up
+        val popupDialog = AlertDialog.Builder(this)
+            .setView(popupView)
+            .create()
+
+        // Set a click listener on the OK button
+        popupView.findViewById<Button>(R.id.validate_button).setOnClickListener {
+            // Retrieve the input value and close the pop-up
+            val dimensionInput = popupView.findViewById<EditText>(R.id.number_input)
+            val dimension = dimensionInput.text.toString().toIntOrNull()
+            if (dimension != null) {
+                drawingView = DrawingView(this, dimension)
+
+                drawingView.setWillNotDraw(false)
+                drawingView.invalidate()
+
+                val container = FrameLayout(this)
+                container.addView(drawingView)
+
+                // Inflate the button layout and add it to the container
+                val buttonLayout = layoutInflater.inflate(R.layout.button_layout, container, false)
+                container.addView(buttonLayout)
+
+                // Set the container as the activity content view
+                setContentView(container)
+
+
+
+                //setContentView(drawingView)
+                popupDialog.dismiss()
+            } else {
+                Toast.makeText(this, "Invalid input", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // Show the pop-up
+        popupDialog.show()
+        }
+
+
+
+        /*super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         drawingView = findViewById<DrawingView>(R.id.vMain)
         drawingView.setWillNotDraw(false)
-        drawingView.invalidate()
+        drawingView.invalidate()*/
 
-    }
+
 }
